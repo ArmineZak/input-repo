dcbf3fb757cd4e7fdfd0af10a57cd330b0786051
ArmineZak <armzak91@gmail.com>
2020-07-02 13:54:02 +0400

    -added impl to be ignored

--- hello-world-with-input/src/HelloWorldWithInput.java
+++ hello-world-with-input/src/HelloWorldWithInput.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorldWithInput {
+    public static void main(String[] args) {
+
+        //Using Scanner for getting input from User
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter hello world, please.");
+        String s = sc.nextLine();
+        System.out.println("You entered:  " + s);
+    }
 }
